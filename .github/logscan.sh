bdff663d20c2402653bf66953082914eff249e19
Daniele Pelagatti <daniele@mono-grid.com>
2023-11-03 13:17:18 +0100

    ci: made typescript checks work

--- .github/logscan.sh
+++ .github/logscan.sh
@@ -1,8 +1,16 @@
 # /bin/bash
 if grep -q error $1; then
   echo "errors in $1"
-  cat $1 | grep error
+  out=$(cat $1)
+  echo "$out"
+
+  # first `sed` replaces newlines with \n
+  # seconds `sed` replaces quotes with escaped quotes \"
+
+  echo "summary=$(echo "$out" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g' )" >> "$GITHUB_OUTPUT"
   exit 1
 fi
-echo "no errors in $1"
+out="no errors in $1"
+echo "$out"
+echo "summary=$out" >> "$GITHUB_OUTPUT"
 exit 0
