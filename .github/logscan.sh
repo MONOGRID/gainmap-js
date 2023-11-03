31b90ad7194613634f212588151bd3dfd0271c4e
Daniele Pelagatti <daniele@mono-grid.com>
2023-11-03 12:12:20 +0100

    test multiline output

--- .github/logscan.sh
+++ .github/logscan.sh
@@ -3,11 +3,18 @@ if grep -q error $1; then
   echo "errors in $1"
   out=$(cat $1)
   echo "$out"
-  # export summary="$out"
-  echo "summary=\"$out\"" >> "$GITHUB_OUTPUT"
+
+  # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#multiline-strings
+  {
+    echo 'summary<<EOF'
+    echo "$out"
+    echo EOF
+  } >> "$GITHUB_OUTPUT"
+
+  # echo "summary=$out" >> "$GITHUB_OUTPUT"
   exit 1
 fi
 out="no errors in $1"
 echo "$out"
-echo "summary=\"$out\"" >> "$GITHUB_OUTPUT"
+echo "summary=$out" >> "$GITHUB_OUTPUT"
 exit 0
