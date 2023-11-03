d9721285bfb20096f196b932bfdc99bb076a371d
Daniele Pelagatti <daniele@mono-grid.com>
2023-11-03 13:00:05 +0100

    produce valid json output in a single line

--- .github/logscan.sh
+++ .github/logscan.sh
@@ -5,13 +5,16 @@ if grep -q error $1; then
   echo "$out"
 
   # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#multiline-strings
-  {
-    echo 'summary<<EOF'
-    echo "$out"
-    echo EOF
-  } >> "$GITHUB_OUTPUT"
+  # {
+  #   echo 'summary<<EOF'
+  #   echo "$out"
+  #   echo EOF
+  # } >> "$GITHUB_OUTPUT"
 
-  # echo "summary=$out" >> "$GITHUB_OUTPUT"
+  # first `sed` replaces newlines with \n
+  # seconds `sed` replaces quotes with escaped quotes \"
+
+  echo "summary=$(echo "$out" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g' )" >> "$GITHUB_OUTPUT"
   exit 1
 fi
 out="no errors in $1"
