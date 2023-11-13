a21f0221fdec46fc6bf0bac45b3e407f12312c84
Daniele Pelagatti <daniele@mono-grid.com>
2023-11-13 10:01:25 +0100

    added 16k test image

--- tests/jsdom/MPFExtractor.test.ts
+++ tests/jsdom/MPFExtractor.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from '@jest/globals'
 import { existsSync } from 'fs'
 import { mkdir, readFile, writeFile } from 'fs/promises'
 import path from 'path'
+import { performance } from 'perf_hooks'
 
 import { MPFExtractor } from '../../src/decode/utils/MPFExtractor'
 
@@ -30,13 +31,20 @@ describe('MPFExtractor', () => {
     { fileName: '09.jpg' },
     { fileName: '10.jpg' },
     { fileName: 'pisa-4k.jpg' },
-    { fileName: 'spruit_sunrise_4k.jpg' }
+    { fileName: 'spruit_sunrise_4k.jpg' },
+    { fileName: 'abandoned_bakery_16k.jpg' }
 
   ])('finds the gainmap in $fileName', async ({ fileName }) => {
     const file = await readFile(path.join(__dirname, `../fixtures/${fileName}`))
 
-    const extractor = new MPFExtractor({ debug: true, extractFII: true, extractNonFII: true })
+    performance.mark(`${fileName} mpf extract start`)
+    const extractor = new MPFExtractor({ extractFII: true, extractNonFII: true })
     const result = await extractor.extract(new Uint8Array(file.buffer))
+    performance.mark(`${fileName} mpf extract end`)
+
+    const measure = performance.measure(`${fileName} mpf extraction`, `${fileName} mpf extract start`, `${fileName} mpf extract end`)
+    console.log(measure.name, measure.duration, 'ms')
+
     expect(result).not.toBeUndefined()
     expect(result).toBeArrayOfSize(2)
 
