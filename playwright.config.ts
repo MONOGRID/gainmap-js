3016d9e09f5c5eac71fcc8f54968090485983a69
Daniele Pelagatti <daniele@mono-grid.com>
2023-11-30 19:47:11 +0100

    playwright report comment in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['junit', { outputFile: 'junit.xml' }]
+    ['json', { outputFile: 'playwright-report.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
