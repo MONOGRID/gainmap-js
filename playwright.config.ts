bc08529c6ed9fb9652ff0e4374ff269052a20cdf
Daniele Pelagatti <daniele@mono-grid.com>
2023-11-30 13:46:01 +0100

    first try, promising

---
+++ playwright.config.ts
@@ -0,0 +1,103 @@
+import { defineConfig, devices } from '@playwright/test'
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  // retries: process.env.CI ? 3 : 0,
+
+  /* Always retry, because some tests are flaky */
+  retries: 3,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['html'],
+    ['junit', { outputFile: 'junit.xml' }]
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://127.0.0.1:8080',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+
+    // Emulates the user locale.
+    locale: 'en-GB',
+
+    // Emulates the user timezone.
+    timezoneId: 'Europe/Paris'
+  },
+
+  // expect timeout to 20 seconds
+  expect: {
+    timeout: 20 * 1000
+  },
+
+  // https://github.com/microsoft/playwright/issues/7575#issuecomment-1693400652
+  // same screenshot name across platforms
+  snapshotPathTemplate: 'tests/__snapshots__/{testFilePath}/{projectName}-{arg}{ext}',
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], launchOptions: { args: ['--font-render-hinting=none'] } }
+    }
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'], launchOptions: { args: ['--font-render-hinting=none'] } },
+    //   testIgnore: ['**/panolens/**'] // FIXME: these tests don't work in this browser
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    //   testIgnore: ['**/panolens/**'] // FIXME: these tests don't work in this browser
+    // }
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run test:startserver',
+    url: 'http://127.0.0.1:8080',
+    reuseExistingServer: !process.env.CI,
+    env: {
+      INSTRUMENT_COVERAGE: 'true'
+    }
+  }
+})
---
+++ tests/testWithCoverage.ts
@@ -0,0 +1,66 @@
+import { test as baseTest } from '@playwright/test'
+import { randomBytes } from 'crypto'
+import { writeFileSync } from 'fs'
+import { mkdir } from 'fs/promises'
+import { join } from 'path'
+
+const istanbulCLIOutput = join(process.cwd(), '.nyc_output')
+
+export function generateUUID (): string {
+  return randomBytes(16).toString('hex')
+}
+
+export const test = baseTest.extend({
+  context: async ({ context }, use) => {
+    await context.addInitScript(() => {
+      /* Deterministic math random */
+      let seed = Math.PI / 4
+      window.Math.random = function () {
+        // const x = window.location.href.split('').map(c => c.charCodeAt(0)).reduce((v,i) => v + i, 0)
+        const x = Math.sin(seed++) * 10000
+        return x - Math.floor(x)
+      }
+
+      /* Collect coverage */
+      window.addEventListener('beforeunload', () =>
+        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__))
+      )
+
+      /* Deterministic Font Rendering across platforms */
+      let styleInjected = false
+      document.addEventListener('readystatechange', (e) => {
+        if (!styleInjected) {
+          styleInjected = true
+          const style = document.createElement('style')
+          style.innerHTML = `
+            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+
+            .VPNav {
+              display: none !important;
+            }
+
+            *, *:before, *:after {
+              font-family: 'Noto Sans' !important;
+              font-size: 16px !important;
+              font-weight: 400 !important;
+              font-kerning: none !important;
+              font-style: normal !important;
+              -webkit-font-smoothing: antialiased !important;
+              text-rendering: geometricprecision !important;
+            }`
+          document.head.append(style)
+        }
+      })
+    })
+    await mkdir(istanbulCLIOutput, { recursive: true })
+    await context.exposeFunction('collectIstanbulCoverage', (coverageJSON: string) => {
+      if (coverageJSON) { writeFileSync(join(istanbulCLIOutput, `playwright_coverage_${generateUUID()}.json`), coverageJSON) }
+    })
+    await use(context)
+    for (const page of context.pages()) {
+      await page.evaluate(() => window.collectIstanbulCoverage(JSON.stringify(window.__coverage__)))
+    }
+  }
+})
+
+export const expect = test.expect
