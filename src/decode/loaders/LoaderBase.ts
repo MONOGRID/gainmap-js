18ad0eca49d95685b7f78fc4137f4aaf1338e782
drcmda <drcmda@gmail.com>
2024-04-02 14:44:58 +0200

    fix: adds setRenderer on LoaderBase (#34)
    WebGLRenderer parameter no longer required in loader constructor, makes loader conform to the three.js Loader standard. The renderer can be set later with `setRenderer`.
     
    Co-authored-by: Daniele Pelagatti <daniele@monogrid.com>

--- src/decode/loaders/LoaderBase.ts
+++ src/decode/loaders/LoaderBase.ts
@@ -20,7 +20,7 @@ import { GainMapDecoderMaterial } from '../materials/GainMapDecoderMaterial'
 import { getHTMLImageFromBlob } from '../utils/get-html-image-from-blob'
 
 export class LoaderBase<TUrl = string> extends Loader<QuadRenderer<typeof HalfFloatType, GainMapDecoderMaterial>, TUrl> {
-  private _renderer: WebGLRenderer
+  private _renderer?: WebGLRenderer
   private _renderTargetOptions?: QuadRendererTextureOptions
   /**
    * @private
@@ -31,10 +31,21 @@ export class LoaderBase<TUrl = string> extends Loader<QuadRenderer<typeof HalfFl
    * @param renderer
    * @param manager
    */
-  constructor (renderer: WebGLRenderer, manager?: LoadingManager) {
+  constructor (renderer?: WebGLRenderer, manager?: LoadingManager) {
     super(manager)
-    this._renderer = renderer
+    if (renderer) this._renderer = renderer
     this._internalLoadingManager = new LoadingManager()
+  }
+
+  /**
+   * Specify the renderer to use when rendering the gain map
+   *
+   * @param renderer
+   * @returns
+   */
+  public setRenderer (renderer: WebGLRenderer) {
+    this._renderer = renderer
+    return this
   }
 
   /**
@@ -53,6 +64,8 @@ export class LoaderBase<TUrl = string> extends Loader<QuadRenderer<typeof HalfFl
    * @returns
    */
   protected prepareQuadRenderer () {
+    if (!this._renderer) console.warn('WARNING: An existing WebGL Renderer was not passed to this Loader constructor or in setRenderer, the result of this Loader will need to be converted to a Data Texture with toDataTexture() before you can use it in your renderer.')
+
     // temporary values
     const material = new GainMapDecoderMaterial({
       gainMapMax: [1, 1, 1],
