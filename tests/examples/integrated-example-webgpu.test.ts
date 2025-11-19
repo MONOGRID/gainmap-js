3e6c762c21307934797129bb6f89570cbe57a2a1
Daniele Pelagatti <daniele@monogrid.com>
2025-11-19 12:24:21 +0100

    test: split integrated example tests

--- tests/examples/integrated-example-webgpu.test.ts
+++ tests/examples/integrated-example-webgpu.test.ts
@@ -3,9 +3,9 @@ import { expect } from '@playwright/test'
 import { test } from '../testWithCoverage'
 import { setupRoutes } from './shared'
 
-test('renders the example correctly', async ({ page, context }) => {
+test('renders the WebGPU example correctly', async ({ page, context }) => {
   await setupRoutes(context)
-  await page.goto('/examples/integrated/', { waitUntil: 'networkidle' })
+  await page.goto('/examples/integrated/webgpu.html', { waitUntil: 'networkidle' })
   await expect(page.locator('canvas').first()).toBeAttached()
   await expect(page).toHaveScreenshot('initial.png')
   await page.mouse.wheel(0, -9000)
